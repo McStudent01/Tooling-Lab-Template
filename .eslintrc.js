0afa1c0ecffdf935717fb5ae25eb9b5fe170a41a
McStudent01 <mcmillcy@mail.uc.edu>
2024-03-24 17:56:37 -0500

    added webpack. replace script import in index.html

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,38 +1,31 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "es2021": true
+  env: {
+    browser: true,
+    es2021: true,
   },
-  "extends": [
-    `eslint:recommended`,
-    `plugin:react/recommended`
-  ],
-  "overrides": [
+  extends: [ `eslint:recommended`, `plugin:react/recommended` ],
+  overrides: [
     {
-      "env": {
-        "node": true
+      env: {
+        node: true,
       },
-      "files": [
-        `.eslintrc.{js,cjs}`
-      ],
-      "parserOptions": {
-        "sourceType": `script`
-      }
-    }
+      files: [ `.eslintrc.{js,cjs}` ],
+      parserOptions: {
+        sourceType: `script`,
+      },
+    },
   ],
-  "parserOptions": {
-    "ecmaVersion": `latest`,
-    "sourceType": `module`
+  parserOptions: {
+    ecmaVersion: `latest`,
+    sourceType: `module`,
   },
-  "plugins": [
-    `react`
-  ],
-  "rules": {
+  plugins: [ `react` ],
+  rules: {
     "array-bracket-spacing": [ 2, `always` ],
     "no-const-assign": 2,
     "no-var": `error`,
-    "indent": [ 2, 2 ],
-    "quotes": [ 2, `backtick` ],
-    "eqeqeq": `error`
-  }
-}
+    indent: [ 2, 2 ],
+    quotes: [ 2, `backtick` ],
+    eqeqeq: `error`,
+  },
+};
