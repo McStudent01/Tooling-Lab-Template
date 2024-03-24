743defc14abdec76ebc72922cb33df991f5a0a08
McStudent01 <mcmillcy@mail.uc.edu>
2024-03-24 16:59:46 -0500

    added eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es2021": true
+  },
+  "extends": [
+    `eslint:recommended`,
+    `plugin:react/recommended`
+  ],
+  "overrides": [
+    {
+      "env": {
+        "node": true
+      },
+      "files": [
+        `.eslintrc.{js,cjs}`
+      ],
+      "parserOptions": {
+        "sourceType": `script`
+      }
+    }
+  ],
+  "parserOptions": {
+    "ecmaVersion": `latest`,
+    "sourceType": `module`
+  },
+  "plugins": [
+    `react`
+  ],
+  "rules": {
+    "array-bracket-spacing": [ 2, `always` ],
+    "no-const-assign": 2,
+    "no-var": `error`,
+    "indent": [ 2, 2 ],
+    "quotes": [ 2, `backtick` ],
+    "eqeqeq": `error`
+  }
+}
